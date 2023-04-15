53bcbda0562f33206489b40ab8f610d20b4198da
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-04-14 23:34:33 -0300

    feat: reset and global styles of project

---
+++ src/pages/index.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Coffee Time</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <main>
+        <h1>Cafeteria</h1>
+      </main>
+    </>
+  );
+}
