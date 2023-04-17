b30b794adcdac85ee61a8d0fc704aea64fb29daa
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-04-16 22:29:25 -0300

    feat: styles details section

--- src/components/Details/index.jsx
+++ src/components/Details/index.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 import styles from './Details.module.scss';
+import Image from 'next/image';
 
 const Details = () => {
-  return <section className={styles.details}>Details</section>;
+  return (
+    <section className={styles.details}>
+      <div className={styles.roast}>
+        <h2>Experimente os diferentes tipos de torra</h2>
+        <div className={styles.types}>
+          <span className={styles.light} />
+          <span className={styles.medium} />
+          <span className={styles.dark} />
+        </div>
+      </div>
+      <Image src='/coffee2.png' width={340} height={225} alt='café' />
+      <div className={styles['card-wrapper']}>
+        <div className={styles.card}>
+          <h2>Coffee Time</h2>
+          <div className={styles.info}>
+            <h3>Endereço</h3>
+            <h4>AV. São José, n' 90 / Canaã dos Carajás - Pará</h4>
+          </div>
+          <div className={styles.info}>
+            <h3>Funcionamento</h3>
+            <h4>
+              <span>Segunda á Sexta: 09 ás 18:00</span>
+              <span>Sábados: 09 ás 14:00</span>
+            </h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Details;
