d4f5f6b69d05b5f9c9f9bb77b67624a6b1e54aea
Werbeth Araujo <werbeth1991pbs@gmail.com>
2023-04-19 15:25:37 -0300

    feat: styles section reviews

--- src/components/Reviews/index.jsx
+++ src/components/Reviews/index.jsx
@@ -1,8 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Reviews.module.scss';
+import Image from 'next/image';
+import Stars from '@/UI/Stars';
+
+const reviews = [
+  {
+    avatar: '/avatar1.png',
+    name: 'Clara Pereira',
+    role: 'Programadora',
+    stars: 5,
+    comment:
+      'Eu simplesmente adorei o café daqui! O ambiente é acolhedor e os funcionários são super atenciosos.',
+  },
+  {
+    avatar: '/avatar2.png',
+    name: 'João Pereira',
+    role: 'Designer',
+    stars: 5,
+    comment:
+      'Adorei o café! É sem dúvida o melhor da cidade. Recomendo a todos que queiram uma experiência única e inesquecível.',
+  },
+  {
+    avatar: '/avatar3.png',
+    name: 'Maria Silva',
+    role: 'Engenheira',
+    stars: 5,
+    comment:
+      'O café é bom, mas acho que poderia ter mais opções de acompanhamentos, como bolos e pães.',
+  },
+];
 
 const Reviews = () => {
-  return <section className={styles.reviews}>Reviews</section>;
+  const [currentReview, setCurrentReview] = useState(0);
+
+  const nextReview = () =>
+    setCurrentReview(
+      currentReview === reviews.length - 1 ? 0 : currentReview + 1
+    );
+  const previousReview = () =>
+    setCurrentReview(
+      currentReview === 0 ? reviews.length - 1 : currentReview - 1
+    );
+
+  useEffect(() => {
+    const interval = setInterval(nextReview, 5000);
+    return () => clearInterval(interval);
+  }, [currentReview]);
+
+  const { avatar, name, role, stars, comment } = reviews[currentReview];
+
+  return (
+    <section className={styles.reviews}>
+      <Image src='/graos.png' alt='graos de café' width={425} height={250} />
+      <h2 className='title light-bg'>O que falam da gente</h2>
+      <div className={styles.review}>
+        <button className={styles.previous} onClick={previousReview}>
+          &larr;
+        </button>
+        <Image
+          className={styles.avatar}
+          src={avatar}
+          alt='image do autor do depoimento'
+          width={120}
+          height={120}
+        />
+        <h3>{name}</h3>
+        <h4>{role}</h4>
+        <Stars stars={stars} />
+        <p>{comment}</p>
+        <button className={styles.next} onClick={nextReview}>
+          &rarr;
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Reviews;
